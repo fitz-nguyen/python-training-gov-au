d495f56ca9eefd615ffad990a1313ed6d0f1f177
Fitz Nguyen <ntuongvn97@gmail.com>
2019-08-01 19:47:45 +0700

    #wip

--- get_data_training_package.py
+++ get_data_training_package.py
@@ -17,10 +17,9 @@ for result in results:
         'title': result.Title,
         'code': result.Code,
         'usage_recommendation': detail['response'].UsageRecommendations.UsageRecommendation[0].State,
-        # 'release_date': detail['response'].UsageRecommendations.UsageRecommendation[0].StartDate
-        'release_date': "2011-6-7"
+        'release_date': str(detail['response'].UsageRecommendations.UsageRecommendation[0].StartDate)
+        # 'release_date': "2011-6-7"
     }
     pprint(data)
     r = requests.post("http://localhost:8088/reg:create-training-package/training-package", json=data)
     print(r.status_code)
-    break
